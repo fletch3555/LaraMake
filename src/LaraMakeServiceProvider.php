7be834f96ca65da0bed519c69e433ea744a55fc9
Eric Fletcher <efletcher@visualdx.com>
2017-04-27 17:00:23 -0400

    Initial commit.  Starting project

---
+++ src/LaraMakeServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace TheFletcher\LaraMake;
+
+use Illuminate\Foundation\Providers\ArtisanServiceProvider;
+
+class LaraMakeServiceProvider extends ArtisanServiceProvider
+{
+
+    /**
+     * The commands to be registered.
+     *
+     * @var array
+     */
+    protected $commands = [
+    ];
+
+    /**
+     * The commands to be registered.
+     *
+     * @var array
+     */
+    protected $devCommands = [
+    ];
+}
